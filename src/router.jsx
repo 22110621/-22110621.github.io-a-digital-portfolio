ef6dedef24a236786ca763b43df733a92a8e4942
JD544 <jakepryce90@gmail.com>
2023-06-18 23:10:43 +0100

    Portfolio files

---
+++ src/router.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+import App from "./App.jsx";
+import Projects from "./projects.jsx";
+import Navigation from "./navigation.jsx";
+import Footer from "./footer.jsx";
+import Project from "./viewProject.jsx";
+import NotFound from "./notFound.jsx";
+import Accessibility from "./components/sidebar.jsx";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />
+    },
+    {
+      path: "/projects",
+      element: <Projects />
+    },
+    {
+      path: "/projects/:project",
+      element: <Project />,
+      errorElement: <NotFound />
+    },
+  ]);
+
+function Routing() {
+    localStorage.getItem("data-theme") ?
+    document.body.setAttribute("data-theme", localStorage.getItem("data-theme")) :
+    document.body.removeAttribute("data-theme")
+  
+    return (
+    <React.StrictMode>
+      <Navigation />
+        <RouterProvider router={router} />
+        <Accessibility />
+      <Footer />
+    </React.StrictMode>
+    )
+}
+
+export default Routing
